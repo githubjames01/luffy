d1cc9dbc6aa6b31cf6e93796b3a27f953c3f369d
冀申 <335268549@qq.com>
2024-04-26 10:34:37 +0800

    admin和轮播广告

---
+++ luffy_api/apps/home/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Banner
+
+
+# Register your models here.
+
+# Blog模型的管理器
+class BannerAdmin(admin.ModelAdmin):
+    list_display=["title","orders","is_show"]
+
+# 在admin中注册绑定
+admin.site.register(Banner, BannerAdmin)
